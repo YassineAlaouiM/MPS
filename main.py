d89b3d32c623a0aa1173fccef69e184bf7d914a3
YassineAlaouiM <yassine.alaoui.m01@gmail.com>
2025-05-07 12:25:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1657,7 +1657,7 @@ def export_schedule():
                 active_shifts.append((shift_key, shift_name))
 
         # Calculate dimensions
-        margin = 40
+        margin = 80
         available_width = page_width - (2 * margin)
         num_columns = len(active_shifts) + 1
         col_width = available_width / num_columns
