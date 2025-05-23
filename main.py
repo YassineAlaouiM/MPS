1271e9de25e39cd26c5e0437d6015ef9b659d664
YassineAlaouiM <yassine.alaoui.m01@gmail.com>
2025-05-23 18:06:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import os
 from dotenv import load_dotenv
 import random
 from reportlab.pdfgen import canvas
-from reportlab.lib.pagesizes import A4, portrait
+from reportlab.lib.pagesizes import A4, portrait, landscape
 from reportlab.lib import colors
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
@@ -1742,17 +1742,14 @@ def export_schedule():
         col_width = available_width / num_columns
         
         # Fixed number of rows per page (10 rows + 1 header row = 11 total)
-        rows_per_page = 15
         row_height = min((page_height - 75) / 12, 50)
 
         # Determine if landscape orientation and 10 rows per page are needed
         landscape_needed = any(shift_key in ('shift_4', 'shift_5', 'shift_6') for shift_key, _ in active_shifts)
         if landscape_needed:
-            from reportlab.lib.pagesizes import landscape
             page_width, page_height = landscape(A4)
             rows_per_page = 10
         else:
-            from reportlab.lib.pagesizes import portrait
             page_width, page_height = portrait(A4)
             rows_per_page = 15
         p = canvas.Canvas(buffer, pagesize=(page_width, page_height))
@@ -1775,10 +1772,8 @@ def export_schedule():
             if page_num > 1:
                 p.showPage()
                 if landscape_needed:
-                    from reportlab.lib.pagesizes import landscape
                     p.setPageSize(landscape(A4))
                 else:
-                    from reportlab.lib.pagesizes import portrait
                     p.setPageSize(portrait(A4))
             add_page_header(p, page_num, total_pages)
 
