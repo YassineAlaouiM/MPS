f79c5d646f6778f7d3ff390d49e1b3a5cd7c7503
YassineAlaouiM <yassine.alaoui.m01@gmail.com>
2025-05-23 17:58:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import os
 from dotenv import load_dotenv
 import random
 from reportlab.pdfgen import canvas
-from reportlab.lib.pagesizes import A4, portrait
+from reportlab.lib.pagesizes import A4, portrait, landscape
 from reportlab.lib import colors
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
@@ -1760,11 +1760,9 @@ def export_schedule():
         # Determine if landscape orientation and 10 rows per page are needed
         landscape_needed = any(shift_key in ('shift_4', 'shift_5', 'shift_6') for shift_key, _ in active_shifts)
         if landscape_needed:
-            from reportlab.lib.pagesizes import landscape
             page_width, page_height = landscape(A4)
             rows_per_page = 10
         else:
-            from reportlab.lib.pagesizes import portrait
             page_width, page_height = portrait(A4)
             rows_per_page = 15
         p = canvas.Canvas(buffer, pagesize=(page_width, page_height))
