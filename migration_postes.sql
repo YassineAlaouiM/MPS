08fa1e5932ee2486e739dbc0670e9670f08ea8c7
YassineAlaouiM <yassine.alaoui.m01@gmail.com>
2025-07-15 10:30:15 +0100

    Update migration_postes.sql

--- migration_postes.sql
+++ migration_postes.sql
@@ -20,19 +20,3 @@ ON DUPLICATE KEY UPDATE name=name;
 
 -- For each operator with postes data, parse the string and create operator_postes entries
 -- Note: This is a simplified migration. You may need to adjust the poste names to match exactly.
-
--- Example migration for common poste names:
--- This will need to be run for each operator that has postes data
-
--- You can run this manually for each operator, or create a more sophisticated script
--- that automatically parses the postes string and creates the appropriate entries.
-
--- Example of how to migrate one operator (replace with actual operator_id and postes):
--- INSERT INTO operator_postes (op_id, poste_id) 
--- SELECT 1, p.id FROM postes p WHERE p.name IN ('machine', 'laveuse');
-
--- To see which operators have postes data:
-SELECT id, name, postes FROM operators WHERE postes IS NOT NULL AND postes != '';
-
--- To see the available postes:
-SELECT * FROM postes ORDER BY name; 
